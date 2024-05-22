616c1dddf9a4aeedcae85022679da6b19557e8b3
Michał Cyran <michalcyran77777@gmail.com>
2024-05-22 21:10:42 +0200

    Breaks fixes

--- src/ui/app.py
+++ src/ui/app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from kivymd.app import MDApp
 from kivymd.uix.menu import MDDropdownMenu
+from kivymd.uix.snackbar import MDSnackbar, MDSnackbarText
 
 from src.model.model_functions import get_movie_recommendations, get_movie_id_by_title, \
     filter_movies, predict_rating, update_user_ratings
@@ -13,7 +14,6 @@ class MovieRecommendationApp(MDApp):
         self.user_movie_matrix = user_movie_matrix
         self.movies = movies
         self.user_means = user_means
-        # self.user_movie_matrix_normalized = user_movie_matrix_normalized
         self.user_id = 2000
         self.user_ratings = {}
 
@@ -23,7 +23,7 @@ class MovieRecommendationApp(MDApp):
         return self.root
 
     def search_movies(self):
-        text = self.root.ids.year_input.text
+        text = self.root.ids.title_input.text
         if text:
             movie_titles = filter_movies(self.movies, text)
         else:
@@ -44,32 +44,31 @@ class MovieRecommendationApp(MDApp):
         self.root.ids.movie_dropdown.dropdown_menu.dismiss()
 
     def submit_rating(self):
-        # user_id_text = self.root.ids.user_id_input.text
-        # if user_id_text:
-        #     user_id = int(user_id_text)
-        # else:
-        #     # Assign new ID
-        #     user_id = self.user_movie_matrix.index.max() + 1
+        movie_title = self.get_title()
+        if movie_title:
+            rating = float(self.root.ids.rating_input.text)
 
-        movie_title = self.root.ids.dropdown_text.text
-        rating = float(self.root.ids.rating_input.text)
+            movie_id = self.movies[self.movies['title'] == movie_title]['movieId'].values[0]
+            self.user_ratings[movie_id] = rating
+            text = f'Rating {rating} submitted for {movie_title}'
 
-        movie_id = self.movies[self.movies['title'] == movie_title]['movieId'].values[0]
-        self.user_ratings[movie_id] = rating
-        # print(f'Movie title: {movie_title}, Movie ID: {movie_id}')
-        #
-        # if user_id in self.user_movie_matrix.index:
-        #     self.user_movie_matrix.loc[user_id, movie_id] = rating
-        # else:
-        #     new_user_ratings = pd.Series(0, index=self.user_movie_matrix.columns)
-        #     new_user_ratings[movie_id] = rating
-        #     self.user_movie_matrix.loc[user_id] = new_user_ratings
-        #
-        # self.root.ids.result_label.text = f'Rating submitted for {movie_title}\nUser ID: {user_id}'
-        # self.update_user_ratings(user_id, movie_id, rating)
+            MDSnackbar(
+                MDSnackbarText(
+                    text=text,
+                ),
+            ).open()
 
     def update_user_ratings(self):
+        if not self.user_ratings:
+            MDSnackbar(
+                MDSnackbarText(
+                    text='Please submit a rating first.',
+                ),
+            ).open()
+            return False
+
         self.user_movie_matrix, self.user_movie_matrix_normalized, self.user_means = update_user_ratings(self.user_id, self.user_ratings, self.user_movie_matrix, self.user_means)
+        return True
 
     def show_recommendations(self):
         # user_id_text = self.root.ids.user_id_input.text
@@ -78,22 +77,37 @@ class MovieRecommendationApp(MDApp):
         # else:
         #     # Assign new ID
         #     user_id = self.user_movie_matrix.index.max() + 1
-        self.update_user_ratings()
-        recommendations = get_movie_recommendations(self.user_id, self.svd, self.user_movie_matrix_normalized, self.movies, self.user_means)
-        recommended_titles = recommendations['title'].tolist()
-        self.root.ids.result_label.text = 'Recommended Movies:\n' + '\n'.join(recommended_titles)
+        if self.update_user_ratings():
+            recommendations = get_movie_recommendations(self.user_id, self.svd, self.user_movie_matrix_normalized, self.movies, self.user_means)
+            recommended_titles = recommendations['title'].tolist()
+            self.root.ids.result_label.text = 'Recommended Movies:\n' + '\n'.join(recommended_titles)
 
     def predict_rating(self):
-        self.update_user_ratings()
-        # user_id = int(self.root.ids.user_id_input.text)
-        movie_title = self.root.ids.dropdown_text.text
-        movie_title = movie_title.split(' (')[0]
-        movie_id = get_movie_id_by_title(movie_title, self.movies)[0]
+        if self.update_user_ratings():
+            # user_id = int(self.root.ids.user_id_input.text)
+            movie_title = self.get_title()
+            if not movie_title:
+                return
 
-        predicted_rating = predict_rating(self.user_id, movie_id, self.svd, self.user_movie_matrix_normalized, self.user_means)
-        if predicted_rating is None:
-            self.root.ids.result_label.text = f'User ID {self.user_id} does not exist. Please submit a rating first.'
-        else:
-            # Scale the predicted rating
-            predicted_rating = max(0.5, min(5.0, predicted_rating))
-            self.root.ids.result_label.text = f'Predicted rating for {movie_title}: {predicted_rating:.2f}'
+            movie_title = movie_title.split(' (')[0]
+            movie_id = get_movie_id_by_title(movie_title, self.movies)[0]
+
+            predicted_rating = predict_rating(self.user_id, movie_id, self.svd, self.user_movie_matrix_normalized, self.user_means)
+            if predicted_rating is None:
+                self.root.ids.result_label.text = f'User ID {self.user_id} does not exist. Please submit a rating first.'
+            else:
+                # Scale the predicted rating
+                predicted_rating = max(0.5, min(5.0, predicted_rating))
+                self.root.ids.result_label.text = f'Predicted rating for {movie_title}: {predicted_rating:.2f}'
+
+    def get_title(self):
+        title = self.root.ids.dropdown_text.text
+        if title == 'Select a Movie':
+            MDSnackbar(
+                MDSnackbarText(
+                    text='Please select a movie first.',
+                ),
+            ).open()
+            return False
+
+        return title
