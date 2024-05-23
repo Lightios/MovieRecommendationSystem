7b1998b9ee919fda210230ecd71c6eb16922dafa
Michał Cyran <michalcyran77777@gmail.com>
2024-05-23 11:23:58 +0200

    UI improved

--- src/ui/app.py
+++ src/ui/app.py
@@ -1,10 +1,15 @@
 import pandas as pd
 from kivy.lang import Builder
 from kivy.properties import StringProperty
+from kivy.uix.widget import Widget
 from kivymd.app import MDApp
 from kivymd.uix.button import MDButton, MDButtonText
 from kivymd.uix.card import MDCard
+from kivymd.uix.dialog import MDDialogButtonContainer, MDDialog, MDDialogHeadlineText, MDDialogSupportingText, \
+    MDDialogContentContainer
+from kivymd.uix.divider import MDDivider
 from kivymd.uix.label import MDLabel
+from kivymd.uix.list import MDListItem, MDListItemSupportingText
 from kivymd.uix.menu import MDDropdownMenu
 from kivymd.uix.snackbar import MDSnackbar, MDSnackbarText
 
@@ -55,7 +60,6 @@ class MovieRecommendationApp(MDApp):
     def submit_rating(self):
         movie_title = self.get_title()
         if movie_title:
-
             rating = float(self.root.ids.rating_input.text)
 
             movie_id = self.movies[self.movies['title'] == movie_title]['movieId'].values[0]
@@ -78,20 +82,57 @@ class MovieRecommendationApp(MDApp):
             ).open()
             return False
 
-        self.user_movie_matrix, self.user_movie_matrix_normalized, self.user_means = update_user_ratings(self.user_id, self.user_ratings, self.user_movie_matrix, self.user_means)
+        self.user_movie_matrix, self.user_movie_matrix_normalized, self.user_means = update_user_ratings(self.user_id,
+                                                                                                         self.user_ratings,
+                                                                                                         self.user_movie_matrix,
+                                                                                                         self.user_means)
         return True
 
     def show_recommendations(self):
-        # user_id_text = self.root.ids.user_id_input.text
-        # if user_id_text:
-        #     user_id = int(user_id_text)
-        # else:
-        #     # Assign new ID
-        #     user_id = self.user_movie_matrix.index.max() + 1
         if self.update_user_ratings():
-            recommendations = get_movie_recommendations(self.user_id, self.svd, self.user_movie_matrix_normalized, self.movies, self.user_means)
+            recommendations = get_movie_recommendations(self.user_id, self.svd, self.user_movie_matrix_normalized,
+                                                        self.movies, self.user_means)
             recommended_titles = recommendations['title'].tolist()
-            self.root.ids.result_label.text = 'Recommended Movies:\n' + '\n'.join(recommended_titles)
+
+            content_items = []
+            # Loop over the recommended_titles
+            for title in recommended_titles:
+                # Create a new MDListItem for each title
+                item = MDListItem(
+                    MDListItemSupportingText(
+                        text=title,
+                    )
+                )
+                # Add the item and a divider to the list
+                content_items.append(item)
+                content_items.append(MDDivider())
+
+            self.dialog = MDDialog(
+                # -----------------------Headline text-------------------------
+                MDDialogHeadlineText(
+                    text="Recommended Movies",
+                ),
+                # -----------------------Supporting text-----------------------
+                MDDialogSupportingText(
+                    text="Our model recommends the following movies for you:",
+                ),
+                # -----------------------Custom content------------------------
+                MDDialogContentContainer(
+                    *content_items,
+                    orientation="vertical",
+                ),
+                # ---------------------Button container------------------------
+                MDDialogButtonContainer(
+                    Widget(),
+                    MDButton(
+                        MDButtonText(text="Close"),
+                        style="text",
+                        on_release=lambda x: self.dialog.dismiss(),
+                    ),
+                ),
+                # -------------------------------------------------------------
+            )
+            self.dialog.open()
 
     def predict_rating(self):
         if self.update_user_ratings():
@@ -103,13 +144,37 @@ class MovieRecommendationApp(MDApp):
             movie_title = movie_title.split(' (')[0]
             movie_id = get_movie_id_by_title(movie_title, self.movies)[0]
 
-            predicted_rating = predict_rating(self.user_id, movie_id, self.svd, self.user_movie_matrix_normalized, self.user_means)
+            predicted_rating = predict_rating(self.user_id, movie_id, self.svd, self.user_movie_matrix_normalized,
+                                              self.user_means)
             if predicted_rating is None:
                 self.root.ids.result_label.text = f'User ID {self.user_id} does not exist. Please submit a rating first.'
             else:
                 # Scale the predicted rating
                 predicted_rating = max(0.5, min(5.0, predicted_rating))
-                self.root.ids.result_label.text = f'Predicted rating for {movie_title}: {predicted_rating:.2f}'
+
+                self.dialog = MDDialog(
+                    # -----------------------Headline text-------------------------
+                    MDDialogHeadlineText(
+                        text="Predicted Rating",
+                    ),
+                    # -----------------------Supporting text-----------------------
+                    MDDialogSupportingText(
+                        text=f'Predicted rating for {movie_title}: {predicted_rating:.2f}',
+                    ),
+                    # ---------------------Button container------------------------
+                    MDDialogButtonContainer(
+                        Widget(),
+                        MDButton(
+                            MDButtonText(text="Close"),
+                            style="text",
+                            on_release=lambda x: self.dialog.dismiss(),
+                        ),
+                    ),
+                    # -------------------------------------------------------------
+                )
+                self.dialog.open()
+
+                # self.root.ids.result_label.text = f'Predicted rating for {movie_title}: {predicted_rating:.2f}'
 
     def get_title(self):
         title = self.selected_movie
@@ -132,5 +197,3 @@ class MovieRecommendationApp(MDApp):
             self.root.ids.rating_label.text = str(self.user_ratings[card.movie_id])
         else:
             self.root.ids.rating_label.text = 'Not rated yet'
-
-
