3ba69199d71e4354a784724ad326eb1ff2456699
Jogurtonelle <kradis27@gmail.com>
2024-06-12 17:46:37 +0200

    model changed

--- src/model/model.py
+++ src/model/model.py
@@ -9,66 +9,159 @@ import numpy as np
 import pickle
 
 # Wczytanie danych
-ratings = pd.read_csv('../ml-latest-small/ratings.csv')
-movies = pd.read_csv('../ml-latest-small/movies.csv')
+ratings = pd.read_csv('src/ml-latest-small/ratings.csv')
+movies = pd.read_csv('src/ml-latest-small/movies.csv')
+tags = pd.read_csv('src/ml-latest-small/tags.csv')
+
+# Usunięcie zbędnych danych - tagów które pojawiają sie tylko raz, oraz kolumn z timestampami i userId
+tags['tag'] = tags['tag'].str.lower()
+tags = tags.groupby('tag').filter(lambda x: len(x) > 1)
+tags = tags.drop(columns=['userId', 'timestamp'])
+
+# Przyporządkowanie każdemu tagowi unikalnego id
+tags['tagId'] = tags['tag'].astype('category').cat.codes
+
+# Zrobienie listy wszystkich tagów i posortowanie po id
+all_tags = tags['tagId'].unique()
+all_tags = np.sort(all_tags)
+
+#Zapisz len(all_tags) do pickle
+#len(all_tags).to_pickle('len_tags.pkl')
+
+# Do każdego filmu dodaj listę rozmiaru all_tags z wartościami od 0 do 1 w zależności od tego czy film ma dany tag
+movie_tags = tags.groupby('movieId')['tagId'].apply(list).reset_index()
+movie_tags['tagId'] = movie_tags['tagId'].apply(lambda x: [2 if i in x else 0 for i in all_tags])
+
+# Połącz dane filmów z tagami - jesli film nie ma tagów to wstaw listę zer
+movies = movies.merge(movie_tags, on='movieId', how='left')
+movies['tagId'] = movies['tagId'].fillna(movies['tagId'].apply(lambda x: [0]*len(all_tags)))
+
+#Zrób listę gatunków
+genres = ['Action', 'Adventure', 'Animation', "Children's", 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy', 'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western', '(no genres listed)']
+
+#len(genres).to_pickle('len_genres.pkl')
+#zrób 2-elementowy dataframe z len(all_tags) i len(genres)
+pd.DataFrame([len(all_tags), len(genres)]).to_pickle('src/model/len_tags_genres.pkl')
+
+#Rozdziel movies['genres'] - podziałką jest '|'
+movies['genres'] = movies['genres'].str.split('|')
+
+#zamien movies['genres'] na listę 0 i 1 w zależności od tego czy film ma dany gatunek
+movies['genres_temp'] = movies['genres'].apply(lambda x: [1 if i in x else 0 for i in genres])
+
+#dodaj listę z gatunkami na początku listy tagID
+movies['tagId'] = movies['genres_temp'] + movies['tagId']
+movies.drop(columns=['genres_temp', 'genres'], inplace=True)
+
+movies.to_pickle('src/model/movies.pkl')
 
 # Tworzenie macierzy użytkownik-film
 user_movie_matrix = ratings.pivot(index='userId', columns='movieId', values='rating')
+user_movie_matrix.to_pickle('src/model/user_movie_matrix.pkl')
 
+# Normalizacja ocen - zamiast NaN wstawmy średnią ocenę filmu
+user_movie_matrix_normalized = user_movie_matrix.fillna(user_movie_matrix.mean(axis=0)).fillna(0)
 # Normalizacja ocen - odejmowanie średniej oceny użytkownika
-user_means = user_movie_matrix.mean(axis=1)
-user_movie_matrix_normalized = user_movie_matrix.sub(user_means, axis=0).fillna(0)
+user_means = user_movie_matrix_normalized.mean(axis=1)
+user_movie_matrix_normalized = user_movie_matrix_normalized.sub(user_means, axis=0).fillna(0)
 
 # Zapisz macierz użytkownik-film i normalizowane średnie
-user_movie_matrix_normalized.to_pickle('user_movie_matrix_normalized.pkl')
-user_means.to_pickle('user_means.pkl')
-movies.to_pickle('movies.pkl')
+user_movie_matrix_normalized.to_pickle('src/model/user_movie_matrix_normalized.pkl')
+user_means.to_pickle('src/model/user_means.pkl')
 
-# Split danych na zbiór treningowy i testowy
-X_train, X_test = train_test_split(user_movie_matrix_normalized, test_size=0.2, random_state=42)
 
-# Pipeline modelu
-pipeline = Pipeline(steps=[
-    ('svd', TruncatedSVD(random_state=42))
-])
 
-# Custom scoring function
-def reconstruction_error(estimator, X):
-    # Pipeline expects 2D array, ensure input is correct
-    if isinstance(X, pd.DataFrame):
-        X = X.values
-    X_transformed = estimator.named_steps['svd'].transform(X)
-    X_reconstructed = estimator.named_steps['svd'].inverse_transform(X_transformed)
-    return mean_squared_error(X, X_reconstructed)
 
-# Siatka parametrów dla GridSearchCV
-param_grid = {
-    'svd__n_components': [10, 20, 30, 40, 50]
-}
 
-# GridSearchCV
-grid_search = GridSearchCV(pipeline, param_grid, cv=5, scoring=make_scorer(reconstruction_error, greater_is_better=False), n_jobs=-1)
-grid_search.fit(X_train)
 
+
+
+
+
+
+
+
+
+
+#compare predictions for test_indexes to real values from user_movie_matrix
+
+#print(test_indexes)
+
+
+##get 10 top rated movies from user 1 from user_movie_matrix
+#user1_ratings = user_movie_matrix.loc[252].sort_values(ascending=False).head(10)
+##to each movieID get title from movies
+#user1_ratings = user1_ratings.reset_index().merge(movies, on='movieId', how='left')
+#print(user1_ratings)
+#
+##get 10 top rated movies from user 1 from user_movie_matrix_reconstructed
+#user1_ratings_reconstructed = user_movie_matrix_reconstructed.loc[252].sort_values(ascending=False).head(10)
+##to each movieID get title from movies
+#user1_ratings_reconstructed = user1_ratings_reconstructed.reset_index().merge(movies, on='movieId', how='left')
+#print(user1_ratings_reconstructed)
+
+# # Normalizacja ocen - odejmowanie średniej oceny użytkownika
+# user_means = user_movie_matrix.mean(axis=1)
+# user_movie_matrix_normalized = user_movie_matrix.sub(user_means, axis=0).fillna(0)
+
+# # Zapisz macierz użytkownik-film i normalizowane średnie
+# #user_movie_matrix_normalized.to_pickle('user_movie_matrix_normalized.pkl')
+# #user_means.to_pickle('user_means.pkl')
+# #movies.to_pickle('movies.pkl')
+
+# # Split danych na zbiór treningowy i testowy
+# X_train, X_test = train_test_split(user_movie_matrix_normalized, test_size=0.2, random_state=42)
+
+# # Pipeline modelu
+# pipeline = Pipeline(steps=[
+#     ('svd', TruncatedSVD(random_state=42))
+# ])
+
+# # Custom scoring function
+# def reconstruction_error(estimator, X):
+#     # Pipeline expects 2D array, ensure input is correct
+#     if isinstance(X, pd.DataFrame):
+#         X = X.values
+#     X_transformed = estimator.named_steps['svd'].transform(X)
+#     X_reconstructed = estimator.named_steps['svd'].inverse_transform(X_transformed)
+#     return mean_squared_error(X, X_reconstructed)
+
+# # Siatka parametrów dla GridSearchCV
+# param_grid = {
+#     'svd__n_components': [10, 20, 30, 40, 50]
+# }
+
+# # GridSearchCV
+# model = Sequential()
+# model.add(Dense(100, activation='relu', input_shape=(X_train.shape[1],)))
+# model.add(Dense(50, activation='relu'))
+# model.add(Dense(10, activation='relu'))
+# model.add(Dense(1, activation='relu'))
+
+# model.compile(loss='mean_squared_error', optimizer='adam')
+
+# model.fit(X_train, X_train, epochs=100, batch_size=10, verbose=2)
+
+# model.evaluate(X_test, X_test)
 # Najlepsze parametry
-best_params = grid_search.best_params_
-print(f'Best parameters: {best_params}')
+# best_params = grid_search.best_params_
+# print(f'Best parameters: {best_params}')
 
-# Najlepszy model
-best_pipeline = grid_search.best_estimator_
+# # Najlepszy model
+# best_pipeline = grid_search.best_estimator_
 
-# Ewaluacja modelu
-X_train_transformed = best_pipeline.named_steps['svd'].transform(X_train)
-X_train_reconstructed = best_pipeline.named_steps['svd'].inverse_transform(X_train_transformed)
-train_mse = mean_squared_error(X_train, X_train_reconstructed)
+# # Ewaluacja modelu
+# X_train_transformed = best_pipeline.named_steps['svd'].transform(X_train)
+# X_train_reconstructed = best_pipeline.named_steps['svd'].inverse_transform(X_train_transformed)
+# train_mse = mean_squared_error(X_train, X_train_reconstructed)
 
-X_test_transformed = best_pipeline.named_steps['svd'].transform(X_test)
-X_test_reconstructed = best_pipeline.named_steps['svd'].inverse_transform(X_test_transformed)
-test_mse = mean_squared_error(X_test, X_test_reconstructed)
+# X_test_transformed = best_pipeline.named_steps['svd'].transform(X_test)
+# X_test_reconstructed = best_pipeline.named_steps['svd'].inverse_transform(X_test_transformed)
+# test_mse = mean_squared_error(X_test, X_test_reconstructed)
 
-print(f'Train Mean Squared Error: {train_mse}')
-print(f'Test Mean Squared Error: {test_mse}')
+# print(f'Train Mean Squared Error: {train_mse}')
+# print(f'Test Mean Squared Error: {test_mse}')
 
 # Zapis najlepszego modelu
-with open('svd_model_best.pkl', 'wb') as f:
-    pickle.dump(best_pipeline, f)
+#with open('svd_model_best.pkl', 'wb') as f:
+#    pickle.dump(best_pipeline, f)
--- src/model/model_functions.py
+++ src/model/model_functions.py
@@ -1,51 +1,5 @@
-import pickle
-
-def load_model(model_path):
-    with open(model_path, 'rb') as f:
-        model = pickle.load(f)
-    return model
-
-
 import numpy as np
 import pandas as pd
-
-
-def get_movie_recommendations(user_id, model, user_movie_matrix, movies, user_means, n_recommendations=10):
-    # Jeśli użytkownik jest nowy lub nie oceniał jeszcze żadnych filmów
-    if user_id not in user_movie_matrix.index or user_movie_matrix.loc[user_id].isnull().all():
-        # Możemy zwrócić najpopularniejsze filmy (np. te z najwyższymi średnimi ocenami)
-        movie_mean_ratings = user_movie_matrix.mean(axis=0)
-        top_movies = movie_mean_ratings.nlargest(n_recommendations).index
-        recommended_movies = movies[movies['movieId'].isin(top_movies)]
-        return recommended_movies
-
-    # Ekstrakcja danych użytkownika
-    user_ratings = user_movie_matrix.loc[user_id].values.reshape(1, -1)
-
-    # Transformacja danych użytkownika
-    user_transformed = model.named_steps['svd'].transform(user_ratings)
-
-    # Rekonstrukcja ocen użytkownika
-    user_reconstructed = model.named_steps['svd'].inverse_transform(user_transformed)
-
-    # Różnica między rekonstrukcją a rzeczywistymi ocenami
-    diff = user_reconstructed - user_ratings
-
-    # Tworzenie ramki danych z różnicami
-    diff_df = pd.DataFrame(diff.flatten(), index=user_movie_matrix.columns, columns=['diff'])
-
-    # Filtrowanie filmów, które użytkownik nie ocenił
-    unseen_movies = diff_df[user_movie_matrix.loc[user_id] == 0]
-
-    # Sortowanie filmów według różnicy w ocenach (im wyższa różnica, tym wyższa rekomendacja)
-    recommendations = unseen_movies.sort_values(by='diff', ascending=False).head(n_recommendations)
-
-    # Łączenie rekomendacji z tytułami filmów
-    recommended_movie_ids = recommendations.index
-    recommended_movies = movies[movies['movieId'].isin(recommended_movie_ids)]
-
-    return recommended_movies
-
 
 def get_movie_id_by_title(title, movies):
     # Szukaj filmu w DataFrame `movies`
@@ -60,30 +14,16 @@ def get_movie_id_by_title(title, movies):
     return movie_ids
 
 
-def predict_rating(user_id, movie_id, model, user_movie_matrix, user_means):
-    # Sprawdzenie, czy film istnieje w macierzy
-    if movie_id not in user_movie_matrix.columns:
-        raise ValueError(f'Movie ID {movie_id} not found in the user-movie matrix.')
+def get_movie_title_by_id(movie_id, movies):
+    # Szukaj filmu w DataFrame `movies`
+    movie = movies[movies['movieId'] == movie_id]
 
-    # Jeśli użytkownik jest nowy lub nie oceniał jeszcze żadnych filmów
-    if user_id not in user_movie_matrix.index or user_movie_matrix.loc[user_id].isnull().all():
-        # Możemy zwrócić średnią ocenę dla filmu
-        overall_movie_mean = user_movie_matrix[movie_id].mean()
-        return overall_movie_mean
+    # Sprawdź, czy znaleziono film
+    if movie.empty:
+        raise ValueError(f'No movie found with ID: {movie_id}')
 
-    # Ekstrakcja danych użytkownika
-    user_ratings = user_movie_matrix.loc[user_id].values.reshape(1, -1)
-
-    # Transformacja danych użytkownika
-    user_transformed = model.named_steps['svd'].transform(user_ratings)
-
-    # Rekonstrukcja ocen użytkownika
-    user_reconstructed = model.named_steps['svd'].inverse_transform(user_transformed)
-
-    # Dodanie średniej oceny użytkownika
-    predicted_rating = user_reconstructed[0, user_movie_matrix.columns.get_loc(movie_id)] + user_means[user_id]
-
-    return predicted_rating
+    # Zwróć tytuł filmu
+    return movie['title'].values[0]
 
 
 def filter_movies(movies, phrase):
@@ -92,16 +32,121 @@ def filter_movies(movies, phrase):
     # return movies[movies['title'].str.contains(str(phrase))]['title'].tolist()
 
 
-def update_user_ratings(user_id, new_ratings, user_movie_matrix, user_means):
+def update_user_ratings(user_id, new_ratings, user_movie_matrix, user_movie_matrix_normalized, user_means):
+    #Do macierzy user_movie_matrix dodajemy srednią ocenę uzytkownika z user_means
+    user_movie_matrix = user_movie_matrix.add(user_means, axis=0)
+
+    #dodaj średnią ocen do user_movie_matrix_normalized dla user_id z user_means
+    user_movie_matrix_normalized.loc[user_id] = user_movie_matrix_normalized.loc[user_id] + user_means[user_id]
+    
     # Dodanie nowych ocen do macierzy
     for movie_id, rating in new_ratings.items():
         user_movie_matrix.loc[user_id, movie_id] = rating
+        #dla filmu o id movie_id oblicz średnią ocenę
+        movie_mean = user_movie_matrix.loc[:, movie_id].mean()
+        #znajdz indeksy wierszy gdzie ocena uzytkownika jest NaN dla filmu o id movie_id
+        nan_indexes = user_movie_matrix.loc[user_movie_matrix[movie_id].isnull()].index
+        #dla tych indeksów wstaw średnią ocenę filmu do user_movie_matrix_normalized
+        user_movie_matrix_normalized.loc[nan_indexes, movie_id] = movie_mean
+
 
     # Aktualizacja średniej oceny użytkownika
     user_means[user_id] = user_movie_matrix.loc[user_id].mean()
 
-    # Normalizacja ocen użytkownika
-    user_movie_matrix_normalized = user_movie_matrix.sub(user_means, axis=0).fillna(0)
+    # Odejmij średnie ocen z user_means od user_movie_matrix_normalized
+    user_movie_matrix_normalized.loc[user_id] = user_movie_matrix_normalized.loc[user_id] + user_means[user_id]
 
     return user_movie_matrix, user_movie_matrix_normalized, user_means
 
+#  last_movie_ratings - dataframe postaci:
+#  last_movie_ratings = pd.DataFrame()
+#  last_movie_ratings['movieId'] = [136020, 1, 166461, 2948, 24]
+#  last_movie_ratings['rating'] = [4, 5, 5, 4, 4]
+#
+# zwraca tablice z movieId filmów
+def get_predictions(user_id, last_movie_ratings, movies, len_genres, len_tags, user_movie_matrix, user_movie_matrix_normalized, user_means):
+    avg_movie = get_average_movie(last_movie_ratings, movies, len_genres, len_tags)
+    result = get_closest_movie(avg_movie, user_id, movies, user_movie_matrix ,20)
+    return get_svd_prediction(user_id, result, user_movie_matrix, movies, user_movie_matrix_normalized, user_means)
+
+
+def get_average_movie(last_movie_ratings, movies, len_genres, len_tags):
+    if len(last_movie_ratings) == 0:
+        return None
+    
+    last_movie_ratings['tagId'] = last_movie_ratings['movieId'].apply(lambda x: movies.loc[movies['movieId'] == x, 'tagId'].values[0])
+
+    # jeśli średnia ocen jest mniejsza niż 2.5 to zwróć tagi odwrotne do tagów filmów
+    if last_movie_ratings['rating'].max() <= 2.5:
+        #get sum of tags
+        tags_sum = np.zeros(len_genres)
+        for tag in last_movie_ratings['tagId']:
+            tags_sum = tags_sum + np.array(tag[:len_genres])
+        
+        #if tags_sum[i] > 0 then make it 0 and if tags_sum[i] == 0 then make it 1
+        tags_sum = [0 if i > 0 else 1 for i in tags_sum]
+        return tags_sum + [0]*len_tags
+    else:
+        #delete films with rating < 2
+        last_movie_ratings = last_movie_ratings[last_movie_ratings['rating'] > 2]
+        #from each rating subtract 2
+        last_movie_ratings['rating'] = last_movie_ratings['rating'] - 2
+
+        ratings_sum = last_movie_ratings['rating'].sum()
+        average_tags = np.zeros(len_genres + len_tags))
+        for id in last_movie_ratings['movieId']:
+            movie_tags = movies.loc[movies['movieId'] == id, 'tagId'].values[0]
+            average_tags = average_tags + np.array(movie_tags) * last_movie_ratings.loc[last_movie_ratings['movieId'] == id, 'rating'].values[0] / ratings_sum
+        return average_tags
+    
+
+def get_closest_movie(tags_array, user_id, movies, user_movie_matrix, n = 20):
+    #copy movieIDs and tags
+    movies_copy = movies[['movieId', 'tagId']].copy()
+
+    #delete movies that user has already rated
+    movies_copy = movies_copy[~movies_copy['movieId'].isin(user_movie_matrix.loc[user_id].dropna().index)]
+    
+    #calculate distance to given average tags
+    movies_copy['dist'] = movies_copy['tagId'].apply(lambda x: np.linalg.norm(np.array(x) - np.array(tags_array)))
+
+    #sort by distance
+    movies_copy = movies_copy.sort_values('dist')
+
+    # add genres to movies_copy - debug
+    #movies_copy['genres'] = movies_copy['tagId'].apply(lambda x: [genres[i] for i in range(len(genres)) if x[i] == 1])
+
+    #get n closest movies
+    return movies_copy.head(n)['movieId'].values
+
+
+def get_svd_prediction(userId, tags_result, user_movie_matrix, movies, user_movie_matrix_normalized, user_means):
+    #wykonaj SVD
+    u, s, vt = np.linalg.svd(user_movie_matrix_normalized, full_matrices=False)
+
+    #reduce dimensionality to 30 biggest singular values
+    k = 30
+    indexes = np.argsort(s)[::-1][:k]
+    u_k = u[:, indexes]
+    s_k = np.diag(s[indexes])
+    vt_k = vt[indexes, :]
+
+    #reconstruct matrix
+    user_movie_matrix_reconstructed = np.dot(u_k, np.dot(s_k, vt_k))
+    #add user means to get original ratings
+    user_movie_matrix_reconstructed = user_movie_matrix_reconstructed + user_means.values[:, np.newaxis]
+    user_movie_matrix_reconstructed = pd.DataFrame(user_movie_matrix_reconstructed, columns=user_movie_matrix.columns, index=user_movie_matrix.index)
+
+
+    #get predictions for movieIDs from result for user 1
+    predictions = user_movie_matrix_reconstructed.loc[userId, tags_result]
+    #add predictions to result
+    tags_result = pd.DataFrame(tags_result, columns=['movieId'])
+    tags_result['prediction'] = predictions.values
+    #sort by prediction
+    tags_result = tags_result.sort_values('prediction', ascending=False)
+    #add titles
+    tags_result = tags_result.merge(movies, on='movieId', how='left')
+    #get top 4 prediction movieID
+    tags_result = tags_result.head(4)['movieId'].values
+    return tags_result
